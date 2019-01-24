6afd7dc8cb180fbf838b8dfe27be0eb54832f494
remiznik <remiznik@gmail.com>
2019-01-25 00:31:04 +0300

    Update vit.py

--- vit.py
+++ vit.py
@@ -3,3 +3,5 @@ memory = {Phosphatidylcholine: [], Huperzine A: [], Optimized Curcumin = []}
 joint = {}
 
 omega = {'C': ['150 мг'], 'A': ['более 700 мкг'] , 'D': ['5-10 мкг (200-400 МЕ)'], 'E': ['не менее 10 мг'], 'цинк': ['женщинам не менее 9 мг, мужчинам – 11'], 'медь': ['1,5 мг'], 'железо': ['мужчинам 15 мг, женщинам не менее 20 мг'], 'сера' = ['около 1 г']}
+
+# http://athleticbody.ru/produkty-soderzhashhie-kollagen.html
